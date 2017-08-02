032ec9709e446ebbbec2656614432c58775dce58
Beracira Chen <beracira.chen@loblaw.ca>
2017-08-02 15:33:46 -0400

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Dependency
+    * python3 3.5+
+    * Pillow
+    * telepot
+
+# Usage
+    replace the token placeholder with your own telegram bot token and run
+        python3 bot.py
+
+# License
+Copyright © 2017 Beracira Chen <g.beracira@gmail>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
---
+++ bot.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+
+import time
+import pprint
+import telepot
+from PIL.Image import open as open_image
+from telepot.loop import MessageLoop
+
+print = pprint.PrettyPrinter().pprint
+
+def handle(msg):
+    print (msg)
+    content_type, chat_type, chat_id = telepot.glance(msg)
+
+    print ('Content type: %s' % content_type)
+
+    if content_type == 'sticker':
+        print ('start downloading...')
+        bot.download_file(msg['sticker']['file_id'], './fin.webp')
+
+        print ('download finished. start converting...')
+        # only gif can be added to wechat sticker pack. 
+        # and it only checks extension of the file but not the actual format
+        open_image('fin.webp').save('fout.gif', 'GIF')
+        print ('conversion done.')
+        
+        fout = open('fout.gif', 'rb')
+        print ('sending photo...')
+        bot.sendDocument(chat_id, fout)
+    else:
+        bot.sendMessage(chat_id, 'I will only accept stickers for now.')
+
+bot = telepot.Bot('***Your Token Here***')
+
+MessageLoop(bot, handle).run_as_thread()
+print ('I am listening ...')
+
+while 1:
+    time.sleep(10)
